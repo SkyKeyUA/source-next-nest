a5d2319be7fa003efb334556437fa32eec005145
SkyKeyUA <veremiienkoov96@gmail.com>
2023-08-02 18:21:34 +0300

    source Next + Nest

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 const start = async () => {
   try {
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule, {cors: false});
+    const PORT = process.env.PORT || 7777;
+    const app = await NestFactory.create(AppModule, { cors: false });
     app.enableCors({
       origin: true,
       credentials: true,
